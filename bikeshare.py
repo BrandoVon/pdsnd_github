26b305a21332b9d2c7f4ff73307dfa6cbfade35b
brandovon <dionysusheero@gmail.com>
2020-05-15 17:34:23 -0500

    Add python code documentation.

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+# The python code for Programming for Data Science with Python
+# -- Introduction to Python Programming class
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
